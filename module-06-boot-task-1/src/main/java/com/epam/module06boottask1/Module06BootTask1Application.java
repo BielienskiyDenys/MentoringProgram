a0f165fda01dae4151b7ca07f41025306530d920
Denys Bielienskyi <denys_bielienskyi@epam.com>
2021-03-29 19:38:40 +0300

    add task01 implementation

---
+++ module-06-boot-task-1/src/main/java/com/epam/module06boottask1/Module06BootTask1Application.java
@@ -0,0 +1,13 @@
+package com.epam.module06boottask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module06BootTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module06BootTask1Application.class, args);
+	}
+
+}
---
+++ module-06-boot-task-1/src/test/java/com/epam/module06boottask1/Module06BootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.module06boottask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module06BootTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
