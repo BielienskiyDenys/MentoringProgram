99e77cef4848b6e3e9c8bd1789e0704e1b98563f
Denys Bielienskyi <denys_bielienskyi@epam.com>
2021-04-14 19:25:47 +0300

    add querries without subtables for two tasks

--- sql/src/main/resources/average_mark_for_subject_name.sql
+++ sql/src/main/resources/average_mark_for_subject_name.sql
@@ -5,3 +5,9 @@ WHERE subject_id IN (
 	SELECT id 
 	FROM subjects 
 	WHERE name = 'subject_101');
+
+	
+SELECT subjects.name, AVG(results.mark) FROM subjects 
+INNER JOIN results on results.subject_id = subjects.id
+GROUP BY subjects.name
+HAVING subjects.name = 'test_subject'; 
--- sql/src/main/resources/red_zone_students.sql
+++ sql/src/main/resources/red_zone_students.sql
@@ -8,6 +8,14 @@ SELECT foo.name, foo.surname FROM (
 ) AS foo WHERE red_zone_marks > 2 /*red zone marks count*/
 LIMIT 1000;
 
+/* Querry without sub-table */
+SELECT students.name, students.surname, SUM(1)
+FROM students 
+INNER JOIN results on results.student_id = students.id
+WHERE mark <= 30
+GROUP BY (students.id)
+HAVING SUM(1) > 6
+LIMIT 1000;
 
 /* count of marks less than X for each student */
 SELECT COUNT(results.id) AS red_zone_marks, students.name, students.surname
@@ -15,6 +23,7 @@ FROM results LEFT JOIN students ON results.student_id = students.id
 WHERE mark<30
 GROUP BY student_id, students.name, students.surname
 LIMIT 100;
+
 
 /*manual check from */
 SELECT * FROM results LIMIT 100;
